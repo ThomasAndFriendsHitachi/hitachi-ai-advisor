bbf17951bd6a62a72a46be35e0e3b79ce48ad170
Lorenzo De Blasio <gioco654@gmail.com>
2026-04-23 18:08:30 +0200

    Started development of key check for webhook events

--- webhook-manager/src/index.js
+++ webhook-manager/src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const redis = require('redis');
 const { v4: uuidv4 } = require('uuid'); //Used to generate UUIDs
+const crypto = require('crypto');
 
 //Express configuration
 const app = express();
 //In case env var are configured
 const port = process.env.PORT || 3000;
+
+const WEBHOOK_SECRET = process.env.GITHUB_WEBHOOK_SECRET || 'my-secret';
 
 //Redis configuration
 const redisUrl = process.env.REDIS_URL || 'redis://redis_broker:6379';
@@ -18,10 +21,34 @@ redisConnection.on('error', (err) => console.error('Error connecting to Redis:',
   console.log('Successfully connected to Redis.');
 })();
 
-app.use(express.json());
+app.use(express.json({
+  verify: (req, res, buf) => {
+    req.rawBody = buf;
+  }
+}));
+// Helper function to verify github signature
+function verifyGitHubSignature(req) {
+  const signature = req.headers['x-hub-signature-256'];
+  if (!signature) return false;
+
+  const hmac = crypto.createHmac('sha256', WEBHOOK_SECRET);
+  const digest = 'sha256=' + hmac.update(req.rawBody).digest('hex');
+
+  try {
+    return crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(digest));
+  } catch (e) {
+    return false;
+  }
+}
 
 //url to use in github action
 app.post('/webhook', async(req, res) => {
+
+  if (!verifyGitHubSignature(req)){
+    console.warn("Received webhook with invalid of missing signature");
+    return res.status(401).send("Invalid signature");
+  }
+
   const payload = req.body;
   const eventType = req.headers['x-github-event'];
 
